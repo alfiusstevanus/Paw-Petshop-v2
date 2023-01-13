4e3a15b6fc6343dcc6eabd476d22cba2d5956e9b
alfiusstevanus <88620306+alfiusstevanus@users.noreply.github.com>
2023-01-13 12:44:25 +0700

    fitur rei

--- src/main/java/com/TugasBesar/view/LoginFormAdmin.java
+++ src/main/java/com/TugasBesar/view/LoginFormAdmin.java
@@ -1,9 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
 package com.TugasBesar.view;
-
 import com.TugasBesar.pojo.Admin;
 import com.TugasBesar.service.AdminService;
 import com.TugasBesar.serviceimpl.AdminServiceImpl;
